1e6892372f01f3911e41c4e1bad5e0c136995d11
Aditya <aditya_pribadi_3@yahoo.com>
2018-01-15 08:49:59 +0700

    edited sketch.js

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ function heuristic(a,b){
 	return d;
 }
 
-var cols = 25;
-var rows = 25;
+var cols = 92;
+var rows = 71;
 var grid = new Array (cols);
 
 var openSet= [];
@@ -35,9 +35,9 @@ function Spot(i,j){
 	
 	//wall is not randomly created
 	//this is created to test, later we will make the real wall
-	if(random(1)<0.3){
-		this.wall = true;
-	}
+//	if(random(1)<0.3){
+//		this.wall = true;
+//	}
 	
 	this.show = function(color){
 		fill(color);
@@ -81,7 +81,7 @@ function Spot(i,j){
 }
 
 function setup(){
-	var myCanvas = createCanvas(600,600);
+	var myCanvas = createCanvas(920,710);
 	myCanvas.parent('myContainer');
 	console.log('A*');
 	
@@ -109,6 +109,426 @@ function setup(){
 	end = grid[cols-1][rows-1];
 	start.wall = false;
 	end.wall = false;
+
+
+	// == \\
+
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][0].wall =true;
+					}		
+	
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][5].wall =true;
+					}
+					for(var i = 35; i < 40; i++){
+						grid[i][5].wall = false;
+					}
+					grid[49][5].wall = false;
+					grid[51][5].wall = false;
+					grid[59][5].wall = false;
+					grid[66][5].wall = false;
+					grid[71][5].wall = false;
+					grid[79][5].wall = false;			
+			
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][10].wall =true;
+					}
+					for(var i = 80; i < 85; i++){
+						grid[i][10].wall = false;
+					}
+					for(var i = 45; i < 50; i++){
+						grid[i][10].wall = false;
+					}
+					for(var i = 85; i < 90; i++){
+						grid[i][10].wall = false;
+					}
+					for(var i = 35; i < 45; i++){
+						grid[i][10].wall = false;
+					}
+					grid[51][10].wall = false;
+					grid[59][10].wall = false;
+			
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][15].wall =true;
+					}	
+					for(var i = 80; i < 85; i++){
+						grid[i][15].wall = false;
+					}
+					
+					for(var i = 40; i < 50; i++){
+						grid[i][15].wall = false;
+					}
+					
+					
+					grid[66][15].wall = false;
+					grid[79][15].wall = false;
+					
+
+				
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][20].wall =true;
+					}
+					for(var i = 80; i < 85; i++){
+						grid[i][20].wall = false;
+					}
+					for(var i = 85; i < 90; i++){
+						grid[i][20].wall = false;
+					}
+					for(var i = 60; i < 65; i++){
+						grid[i][20].wall = false;
+					}
+					for(var i = 40; i < 50; i++){
+						grid[i][20].wall = false;
+					}
+					for(var i = 35; i < 40; i++){
+						grid[i][20].wall = false;
+					}
+					
+					grid[51][20].wall = false;
+					grid[59][20].wall = false;
+					grid[66][20].wall = false;
+					grid[71][20].wall = false;
+					grid[79][20].wall = false;
+
+
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][25].wall =true;
+					}	
+					for(var i = 80; i < 85; i++){
+						grid[i][25].wall = false;
+					}
+					for(var i = 60; i < 65; i++){
+						grid[i][25].wall = false;
+					}
+					for(var i = 85; i < 90; i++){
+						grid[i][25].wall = false;
+					}
+				
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][30].wall =true;
+					}
+					for(var i = 80; i < 85; i++){
+						grid[i][30].wall = false;
+					}
+					for(var i = 85; i < 90; i++){
+						grid[i][30].wall = false;
+					}
+					for(var i = 60; i < 65; i++){
+						grid[i][30].wall = false;
+					}
+					for(var i = 35; i < 60; i++){
+						grid[i][30].wall = false;
+					}
+					
+					
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][35].wall =true;
+					}
+					for(var i = 80; i < 85; i++){
+						grid[i][35].wall = false;
+					}
+					for(var i = 60; i < 65; i++){
+						grid[i][35].wall = false;
+					}
+					for(var i = 85; i < 90; i++){
+						grid[i][35].wall = false;
+					}
+					grid[51][35].wall = false;
+					grid[59][35].wall = false;
+					grid[66][35].wall = false;
+					grid[71][35].wall = false;
+					
+									
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][40].wall =true;
+					}
+					for(var i = 80; i < 85; i++){
+						grid[i][40].wall = false;
+					}
+					for(var i = 50; i < 55; i++){
+						grid[i][40].wall = false;
+					}
+					for(var i = 40; i < 50; i++){
+						grid[i][40].wall = false;
+					}
+					grid[59][40].wall = false;
+					grid[61][40].wall = false;
+					grid[66][40].wall = false;
+					grid[71][40].wall = false;
+					grid[79][40].wall = false;
+					
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][45].wall =true;
+					}
+					for(var i = 80; i < 85; i++){
+						grid[i][45].wall = false;
+					}
+					for(var i = 50; i < 55; i++){
+						grid[i][45].wall = false;
+					}
+					for(var i = 40; i < 50; i++){
+						grid[i][45].wall = false;
+					}
+					for(var i = 85; i < 90; i++){
+						grid[i][45].wall = false;
+					}
+										
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][50].wall =true;
+					}
+					for(var i = 80; i < 85; i++){
+						grid[i][50].wall = false;
+					}
+					for(var i = 50; i < 55; i++){
+						grid[i][50].wall = false;
+					}
+					for(var i = 45; i < 50; i++){
+						grid[i][50].wall = false;
+					}	
+					grid[59][50].wall = false;
+					grid[61][50].wall = false;
+					grid[66][50].wall = false;
+					grid[71][50].wall = false;
+					grid[79][50].wall = false;
+					
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][55].wall =true;
+					}
+					for(var i = 85; i < 90; i++){
+						grid[i][55].wall = false;
+					}
+					grid[51][55].wall = false;
+					grid[71][55].wall = false;
+					grid[79][55].wall = false;
+					
+	for(var i = cols-1; i >= cols-90; i--){
+						grid[i][60].wall =true;
+					}
+
+
+							
+	
+			//  ||	\\	
+					
+					
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[90][i].wall =true;
+			}	
+			
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[85][i].wall =true;
+			}
+			for(var i = 1; i < 5; i++){
+				grid[85][i].wall = false;
+			}
+			grid[85][6].wall = false;
+			grid[85][21].wall = false;
+			grid[85][41].wall = false;
+			grid[85][51].wall = false;
+
+						
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[80][i].wall =true;
+			}
+			for(var i = 6; i < 10; i++){
+				grid[80][i].wall = false;
+			}
+			for(var i = 1; i < 5; i++){
+				grid[80][i].wall = false;
+			}
+			for(var i = 16; i < 20; i++){
+				grid[80][i].wall = false;
+			}
+			for(var i = 36; i < 40; i++){
+				grid[80][i].wall = false;
+			}
+			for(var i = 51; i < 55; i++){
+				grid[80][i].wall = false;
+			}
+			for(var i = 56; i < 60; i++){
+				grid[80][i].wall = false;
+			}
+			grid[80][29].wall = false;
+
+
+
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[75][i].wall =true;
+			}
+			for(var i = 6; i < 10; i++){
+				grid[75][i].wall = false;
+			}
+			for(var i = 16; i < 20; i++){
+				grid[75][i].wall = false;
+			}
+			for(var i = 36; i < 40; i++){
+				grid[75][i].wall = false;
+			}
+			for(var i = 51; i < 55; i++){
+				grid[75][i].wall = false;
+			}
+			for(var i = 26; i < 30; i++){
+				grid[75][i].wall = false;
+			}
+			for(var i = 31; i < 35; i++){
+				grid[75][i].wall = false;
+			}
+			
+		
+				
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[70][i].wall =true;
+			}
+			for(var i = 6; i < 10; i++){
+				grid[70][i].wall = false;
+			}
+			for(var i = 16; i < 20; i++){
+				grid[70][i].wall = false;
+			}
+			for(var i = 11; i < 15; i++){
+				grid[70][i].wall = false;
+			}
+			for(var i = 36; i < 40; i++){
+				grid[70][i].wall = false;
+			}
+			for(var i = 51; i < 55; i++){
+				grid[70][i].wall = false;
+			}
+			for(var i = 56; i < 60; i++){
+				grid[70][i].wall = false;
+			}
+
+	
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[65][i].wall =true;
+			}	
+			for(var i = rows-62; i >= rows-65; i--){
+				grid[65][i].wall =false;
+			}
+			for(var i = 16; i < 20; i++){
+				grid[65][i].wall = false;
+			}
+			for(var i = 36; i < 40; i++){
+				grid[65][i].wall = false;
+			}
+			for(var i = 51; i < 55; i++){
+				grid[65][i].wall = false;
+			}
+			for(var i = 56; i < 60; i++){
+				grid[65][i].wall = false;
+			}
+			grid[65][29].wall = false;
+
+					
+
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[60][i].wall =true;
+			}
+			for(var i = rows-62; i >= rows-65; i--){
+				grid[60][i].wall =false;
+			}
+			for(var i = 1; i < 5; i++){
+				grid[60][i].wall = false;
+			}
+			for(var i = 16; i < 20; i++){
+				grid[60][i].wall = false;
+			}
+			for(var i = 11; i < 15; i++){
+				grid[60][i].wall = false;
+			}
+			for(var i = 36; i < 40; i++){
+				grid[60][i].wall = false;
+			}
+			for(var i = 51; i < 55; i++){
+				grid[60][i].wall = false;
+			}
+			for(var i = 56; i < 60; i++){
+				grid[60][i].wall = false;
+			}
+
+				
+
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[55][i].wall =true;
+			}
+			
+			for(var i = 6; i < 10; i++){
+				grid[55][i].wall = false;
+			}
+			for(var i = 16; i < 20; i++){
+				grid[55][i].wall = false;
+			}
+			for(var i = 36; i < 40; i++){
+				grid[55][i].wall = false;
+			}
+			for(var i = 51; i < 55; i++){
+				grid[55][i].wall = false;
+			}
+			
+					
+
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[50][i].wall =true;
+			}
+			for(var i = 6; i < 10; i++){
+				grid[50][i].wall = false;
+			}
+			for(var i = 16; i < 20; i++){
+				grid[50][i].wall = false;
+			}
+			for(var i = 26; i < 35; i++){
+				grid[50][i].wall = false;
+			}
+			for(var i = 51; i < 55; i++){
+				grid[50][i].wall = false;
+			}
+			for(var i = 56; i < 60; i++){
+				grid[50][i].wall = false;
+			}
+			
+
+			
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[45][i].wall =true;
+			}
+			for(var i = 1; i < 5; i++){
+				grid[45][i].wall = false;
+			}
+			for(var i = 6; i < 11; i++){
+				grid[45][i].wall = false;
+			}
+			for(var i = 11; i < 15; i++){
+				grid[45][i].wall = false;
+			}
+			for(var i = 15; i < 20; i++){
+				grid[45][i].wall = false;
+			}
+			for(var i = 20; i < 25; i++){
+				grid[45][i].wall = false;
+			}
+			for(var i = 36; i < 50; i++){
+				grid[45][i].wall = false;
+			}
+			grid[45][52].wall = false;
+
+			
+			
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[40][i].wall =true;
+			}
+			for(var i = 26; i < 35; i++){
+				grid[40][i].wall = false;
+			}
+			grid[40][6].wall = false;
+			grid[40][21].wall = false;
+			grid[40][49].wall = false;
+
+			
+	for(var i = rows-1; i >= rows-70; i--){
+				grid[35][i].wall =true;
+			}
+			
+	
 	
 	openSet.push(start);
 	
